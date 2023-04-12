0f18ddec81d8800af6c3671af3dfd4e73e8932b9
Choirandvice <ryan.thomas.harrison@gmail.com>
2023-04-12 14:54:29 +0930

    1.6 initial commit

---
+++ part1/exercise1.14/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  const setGoodValueTo = (newValue) => {
+    console.log('value good now', newValue)
+    setGood(newValue)
+  }
+
+  const setNeutralValueTo = (newValue) => {
+    console.log('value neutral now', newValue)
+    setNeutral(newValue)
+  }
+
+  const setBadValueTo = (newValue) => {
+    console.log('value bad now', newValue)
+    setBad(newValue)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>    
+      <Button handleClick={() => setGoodValueTo(good + 1)} text="good" />
+      <Button handleClick={() => setNeutralValueTo(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBadValueTo(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/exercise1.5/src/App.js
@@ -0,0 +1,71 @@
+const Header = ({name}) => {
+  console.log(name)
+
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({part}) => {
+  const {name,exercises} = part
+
+  console.log(part)
+
+  // console.log(part)
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+
+}
+
+const Content = ({parts}) => {
+  console.log(parts)
+  return (
+    <div>
+      <Part part={parts[0]}/>
+      <Part part={parts[1]}/>
+      <Part part={parts[2]}/>
+    </div>
+  )
+}
+
+const Total = ({parts}) => {
+  console.log(parts)
+  return (
+    <p>
+      Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+      {
+        name:'Fundamentals of React', 
+        exercises:10
+      },
+      {
+        name:'Using props to pass data', 
+        exercises:7
+      },
+      {
+        name:'State of a component', 
+        exercises:14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default App
