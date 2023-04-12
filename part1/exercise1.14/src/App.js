ea037321c1fd85d148b36fae3298cff0063f0834
Choirandvice <ryan.thomas.harrison@gmail.com>
2023-04-12 17:28:50 +0930

    1.8 commit

--- part1/exercise1.14/src/App.js
+++ part1/exercise1.14/src/App.js
@@ -6,6 +6,21 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = ({good,bad,neutral}) => {
+  return (
+    <div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {good+neutral+bad}</p>
+      <p>average {(good*1 + bad*-1)/(good+neutral+bad)}</p>
+      <p>positive {100*(good)/(good+neutral+bad)}%</p>
+    </div>
+  )
+}
+
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -14,8 +29,6 @@ const App = () => {
 
   const total = 0
   const average = 0
-
-
 
   const setGoodValueTo = (newValue) => {
     console.log('value good now', newValue)
@@ -38,13 +51,9 @@ const App = () => {
       <Button handleClick={() => setGoodValueTo(good + 1)} text="good" />
       <Button handleClick={() => setNeutralValueTo(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBadValueTo(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good+neutral+bad}</p>
-      <p>average {(good*1 + bad*-1)/(good+neutral+bad)}</p>
-      <p>positive {100*(good)/(good+neutral+bad)}%</p>
+
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+
     </div>
   )
 }
